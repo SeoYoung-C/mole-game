2ce572f7b7ce5a376606044940c3c62c52008a37
seoyoung.choi <cseoyoingna@gmail.com>
2022-08-16 20:30:11 +0900

    refac: visible event handle and function names

--- src/pages/play/hole-event/visible-hole-event.ts
+++ src/pages/play/hole-event/visible-hole-event.ts
@@ -2,32 +2,32 @@ import { MOLE_PERCENT, BOM_PERVENT } from 'constants/percent';
 
 import { SetTimeoutRefType } from '../interface';
 
-const mutateTimeout = (button: Element, classList: DOMTokenList) => {
+const hiddenItem = (button: Element, classList: DOMTokenList) => {
 	classList.add('hidden');
 	button.setAttribute('disabled', '');
 };
 
-const mutateTimeIn = (button: Element, classList: DOMTokenList) => {
+const visibleItem = (button: Element, classList: DOMTokenList) => {
 	classList.remove('hidden');
 	button.removeAttribute('disabled');
 };
 
-const hiddenHoleControl = (index: number, button: Element, mole: Element, bomb: Element, levelTime: number) => {
+const handleMoleOrBomb = (index: number, button: Element, mole: Element, bomb: Element, levelTime: number) => {
 	const randomValue = Math.random();
 
 	if (randomValue < MOLE_PERCENT) {
 		const newMole = {
-			[index]: setTimeout(() => mutateTimeout(button, mole.classList), levelTime)
+			[index]: setTimeout(() => hiddenItem(button, mole.classList), levelTime)
 		};
-		mutateTimeIn(button, mole.classList);
+		visibleItem(button, mole.classList);
 		return newMole;
 	}
 
 	if (MOLE_PERCENT < randomValue && randomValue < BOM_PERVENT) {
 		const newBomb = {
-			[index]: setTimeout(() => mutateTimeout(button, bomb.classList), levelTime)
+			[index]: setTimeout(() => hiddenItem(button, bomb.classList), levelTime)
 		};
-		mutateTimeIn(button, bomb.classList);
+		visibleItem(button, bomb.classList);
 		return newBomb;
 	}
 
@@ -46,17 +46,20 @@ export const visibleHoleEvent = (
 	if (holeButtonList) {
 		const timeOutList = holeIndexList.map((item, index) => {
 			const buttonElement = holeButtonList[item];
-			const buttonElementIndex = holeButtonList[item]?.id.replace('hole-button-', '');
+			const buttonElementIndex = buttonElement?.id.replace('hole-button-', '');
 
-			const mole = buttonElement?.querySelector(`#mole-${buttonElementIndex}`);
-			const bomb = buttonElement?.querySelector(`#bomb-${buttonElementIndex}`);
+			const moleElement = buttonElement?.querySelector(`#mole-${buttonElementIndex}`);
+			const bombElement = buttonElement?.querySelector(`#bomb-${buttonElementIndex}`);
 
 			if (item === 0) {
 				return { [index]: 0 };
 			}
 
-			if (mole?.classList.contains('hidden') && bomb?.classList.contains('hidden')) {
-				return hiddenHoleControl(index, buttonElement, mole, bomb, levelTime);
+			if (
+				moleElement?.classList.contains('hidden') === true &&
+				bombElement?.classList.contains('hidden') === true
+			) {
+				return handleMoleOrBomb(index, buttonElement, moleElement, bombElement, levelTime);
 			}
 
 			return holesEventsList?.[index];
